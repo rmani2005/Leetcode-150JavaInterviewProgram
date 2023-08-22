f67bde50592d2df5ff6d37f720bdec900abab051
Manikandan Ravi <rmani2005@gmail.com>
2023-08-22 16:00:35 +0000

    Code push

---
+++ mergeSortedArraytype2.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+class mergeSortedArraytype2 
+{
+
+    public static void main(String[] args) {
+
+        int[] num1={1,2,3};int[] num2={1,2};
+        int[] output= merge(num1,num1.length,num2,num2.length);
+        for (int i : output) {
+            System.out.println(i);
+        }
+    }
+
+    static public int[] merge(int[] nums1, int m, int[] nums2, int n) 
+    {
+        
+
+        //Dont complicate first, if merge means we need to thing in easy way to merge using for loop
+        for(int i=0;i<n;i++)
+        {
+                nums1[m]=nums2[i];
+                m++;
+        }
+
+        Arrays.sort(nums1);
+       //System.out.println(nums1.toString());
+        return nums1;
+    }
+}
